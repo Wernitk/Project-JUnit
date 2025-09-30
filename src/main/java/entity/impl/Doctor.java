1d16d81408a0181e6e72f13fa16340bc3712f00f
mykytasirobaba <mykyta.sirobaba@gmail.com>
2025-09-30 20:35:42 +0300

    new 2

--- src/main/java/entity/impl/Doctor.java
+++ src/main/java/entity/impl/Doctor.java
@@ -1,8 +1,36 @@
 package entity.impl;
 
-/**
- * Created by Mykyta Sirobaba on 30.09.2025.
- * email mykyta.sirobaba@gmail.com
- */
+import java.util.Objects;
+
 public class Doctor {
+    private String name;
+    private double money;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Doctor doctor = (Doctor) o;
+        return Double.compare(money, doctor.money) == 0 && Objects.equals(name, doctor.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, money);
+    }
 }
